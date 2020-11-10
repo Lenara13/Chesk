bdf08af78cb027f790dd766b791d6d423df42349
Lenara13 <ilenara@list.ru>
2020-11-10 23:03:10 +0500

    Добавьте файлы проекта.

---
+++ Chesk/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Chesk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском языке с маленькой буквы");
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("You color is red");
+            }
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("You color is red");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("You color is cyan");
+            }
+        }
+    }
+}
